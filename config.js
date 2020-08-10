3b1405fcb73691dd4db53cea5d49d76d2f413836
Alexey Dikarev <mailsub@yandex.ru>
2020-08-11 00:05:17 +0300

    refactor: assign process.env.DB_HOST if production env

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const { NODE_ENV } = process.env;
 
-const DB_HOST = process.env.DB_HOST || 'mongodb://localhost:27017/newsdb';
+const DB_HOST = NODE_ENV === 'production'
+  ? process.env.DB_HOST
+  : 'mongodb://localhost:27017/newsdb';
 
 const JWT_SECRET = NODE_ENV === 'production'
   ? process.env.JWT_SECRET
