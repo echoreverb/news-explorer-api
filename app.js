a7e9498cb47ab5dce4d825eaaad8e7047cf2c68c
Alexey Dikarev <mailsub@yandex.ru>
2020-09-04 17:53:38 +0300

    fix: fix auth middleware to check jwt cookie

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
@@ -29,6 +30,7 @@ app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(cors(corsOptions));
 
